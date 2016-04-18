c569ca6faea318ba0fe526593b3058ef63bd1f1e
powolnymarcel <powolnymarcel@gmail.com>
2016-04-18 16:50:44 +0200

    premier commit du projet laravel-social

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,44 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.master')
 
+@section('title')
+    titre
+    @endsection
 
-    </head>
-    <body>
+@section('contenu')
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Inscription</h3>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="email">Votre email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="nom">Votre Nom</label>
+                    <input type="text" name="nom" id="nom" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Votre mot de passe</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <button class="btn btn-default" type="submit">Envoyer</button>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+            </form>
+        </div>
 
-    </body>
-</html>
+        <div class="col-md-6">
+            <h3>Connexion</h3>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Votre email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                                <div class="form-group">
+                    <label for="password">Votre mot de passe</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <button class="btn btn-default" type="submit">Envoyer</button>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+            </form>
+        </div>    </div>
+@endsection
